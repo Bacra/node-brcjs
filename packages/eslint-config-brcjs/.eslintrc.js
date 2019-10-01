b5a2a6631fb6196e18b25aef76748a683625520a
bacrawu <bacrawu@tencent.com>
2019-10-01 15:57:29 +0800

    Add: es6 support

--- packages/eslint-config-brcjs/.eslintrc.js
+++ packages/eslint-config-brcjs/.eslintrc.js
@@ -1,17 +1,33 @@
 module.exports = {
-	env: { node: true },
+	parser: 'babel-eslint',
+	env: {
+		node: true,
+		es6: true,
+	},
 	root: true,
 	extends: 'eslint:recommended',
+	globals: {
+		self: false,
+		global: false
+	},
+	parserOptions: {
+		ecmaVersion: 2017,
+		sourceType: 'module',
+		allowImportExportEverywhere: false,
+		ecmaFeatures: {
+			experimentalObjectRestSpread: true,
+			jsx: true,
+			modules: true
+		}
+	},
 	rules: {
 		'no-console': 'off',
 		'no-redeclare': 'off',
 	},
-	overrides: [
-		{
-			files: ['**/test/*.js'],
-			env: {
-				mocha: true
-			}
+	overrides: [{
+		files: ['**/test/*.js'],
+		env: {
+			mocha: true
 		}
-	],
+	}],
 };
---
+++ packages/eslint-config-brcjs/README.md
@@ -0,0 +1,15 @@
+# ESLINT-CONFIG-BRCJS
+
+## Install
+
+`npm install eslint babel-eslint eslint-config-brcjs --save-dev`
+
+## Useage
+
+Create file `.eslintrc.js`
+
+```javascript
+module.exports = {
+    extends: 'eslint-config-brcjs'
+};
+```
